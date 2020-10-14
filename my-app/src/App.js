d33664ebb805bc12d75a5e1321f5efdae256af07
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-10-14 18:02:09 -0400

    mvp completed

---
+++ my-app/src/App.js
@@ -0,0 +1,101 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Form from "./components/Form";
+import axios from "axios";
+import * as yup from "yup";
+import schema from "./validation/formSchema";
+import Users from "./components/Users"
+
+const initialFormValues = {
+  username: "",
+  email: "",
+  password: "",
+  tos: false
+}
+const initialDisabled = true;
+const initialFormErrors = {
+  username: "",
+  email: "",
+  password: "",
+  
+};
+function App() {
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors); 
+  const [disabled, setDisabled] = useState(initialDisabled); 
+  const [users, setUsers] = useState([])
+
+    const formSubmit = () =>{
+      let newUser = {
+        username: formValues.username.trim(),
+        email: formValues.email.trim(),
+        password: formValues.password,
+        tos: formValues.tos
+      }
+      
+       setUsers([ ...users, newUser ])
+      // console.log(users)
+      // console.log(newUser)
+      postNewUser(newUser)
+    }
+    const postNewUser = (newUser) => {
+    
+    axios
+      .post("https://reqres.in/api/users", newUser)
+      .then((res) => {
+        // console.log(res)
+        setUsers([res.data, ...users]);
+        setFormValues(initialFormValues);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+
+
+
+ const inputChange = (name, value) => {
+
+    yup
+      .reach(schema, name) 
+      .validate(value) 
+      .then(() => {
+        
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      }) 
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          
+          [name]: err.errors[0],
+        });
+      });
+    setFormValues({
+      ...formValues,
+      [name]: value, 
+    });
+  };
+    useEffect(() => {
+    schema.isValid(formValues).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [formValues]);
+  return (
+    <div className="App">
+      <h1>from app</h1>
+      <Form values={formValues} change={inputChange} disabled={disabled} errors={formErrors} submit={formSubmit} />
+
+      {users.map((item)=>{
+        {console.log(item)}
+        return  <Users data={item}/>
+      })}
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/validation/formSchema.js
@@ -0,0 +1,16 @@
+import * as yup from "yup";
+
+export default yup.object().shape({
+  username: yup
+    .string()
+    .required("name is required")
+    .min(3, "username must be atleast 3 characters"),
+  email: yup
+    .string()
+    .email("must be a valid email address")
+    .required("email is required"),
+  password: yup
+    .string()
+    .required("password is required"),
+  tos: yup.boolean(),
+});
